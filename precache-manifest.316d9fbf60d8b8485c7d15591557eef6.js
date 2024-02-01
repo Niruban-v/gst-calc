f86a82fe69c289fcfdfc515363ab99b74e318f03
Niruban V <nirubanv@ORBTECH.COM>
2024-02-01 09:18:27 +0530

    Updates

---
+++ precache-manifest.316d9fbf60d8b8485c7d15591557eef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daa8a275981984f2e329a408454e9b8",
+    "url": "/gst-calc/index.html"
+  },
+  {
+    "revision": "b946aa86f9497b9f8db4",
+    "url": "/gst-calc/static/css/main.3f4225ed.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/gst-calc/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/gst-calc/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "b946aa86f9497b9f8db4",
+    "url": "/gst-calc/static/js/main.ffeb542c.chunk.js"
+  },
+  {
+    "revision": "6f50dab6f132baef66fb",
+    "url": "/gst-calc/static/js/runtime~main.d557576d.js"
+  }
+]);
